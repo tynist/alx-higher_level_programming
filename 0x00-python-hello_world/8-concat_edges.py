c86743df934de321cec3f54a945345566476c693
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2022-09-19 16:00:04 +0100

    commits

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programmng\
-        language that combines remarkable power with very clear syntax"
+str = "Python is an interpreted, interactive, object-oriented programmng\language that combines remarkable power with very clear syntax"
 str = str[39:66] + str[-23:-17] + str[:6]
 print(str)
