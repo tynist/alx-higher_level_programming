7f018a9b7776a2346fd5159cab4e584734ce9c1e
tynist <ximonaka@gmail.com>
2022-09-20 09:14:34 -0700

    commits

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def magic_calculation(a, b, c):
+    ""a function that does exactly the same as the following Python bytecode
+    """
+    if a < b:
+        return c
+    if c > b:
+        return a + b
+    return (a * b) - c
