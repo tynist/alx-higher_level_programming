abc94d4cbe3feba10cc9c3f0c4d49674c73e4d14
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2022-09-20 14:21:14 +0100

    commits

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,4 +4,4 @@ for d1 in range(0, 10):
         if d1 == 8 and d2 == 9:
             print("{}{}".format(d1, d2), end=", ")
         else:
-            print("{}{}".format(d1, d2), end="\n")
+            print("{}{}".format(d1, d2))
