69736d50b78ff18ed1df8aaf5bb16489496137cc
tynist <ximonaka@gmail.com>
2022-09-22 05:31:31 -0700

    commits

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    """Does some Math, print the result  of 10 and 5."""
+    """Does some maths, print the result  of 10 and 5."""
     from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
