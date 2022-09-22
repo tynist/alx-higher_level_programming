435ea793b232e9d349a2a35b6dcabdaa809a61ce
tynist <ximonaka@gmail.com>
2022-09-22 06:01:15 -0700

    commits

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,13 +3,12 @@ if __name__ == "__main__":
     """Print the number of and list of arguments"""
     import sys
     total = len(sys.argv)
-
     if total <= 1:
         print("0 arguments.")
     else:
         if total == 2:
-            print("{} argument:".format(total - 1))
+            print("{:d} argument:".format(total - 1))
         else:
-            print("{} arguments:".format(total - 1))
+            print("{:d} arguments:".format(total - 1))
         for i in range(1, total):
-            print("{}: {}".format(i, sys.argv[i]))[i]))
+            print("{:d}: {}".format(i, sys.argv[i]))
