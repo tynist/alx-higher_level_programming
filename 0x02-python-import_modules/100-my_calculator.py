14c285119a8ab3b7160b91e156d97187c0cf4709
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2022-09-22 14:48:51 +0100

    commits

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,19 +4,19 @@ if __name__ == "__main__":
     basic operations."""
     from calculator_1 import add, sub, mul, div
     import sys
-    
+
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    
+
     ops = sys.argv[2]
     if ops not in '*+-/':
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
-    
+
     a = int(sys.argv[1])
     b = int(sys.argv[3])
-    
+
     if ops == '+':
         print("{} + {} = {}".format(a, b, add(a, b)))
     elif ops == '-':
