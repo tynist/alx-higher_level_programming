86c5acb4cd930c5edea6b2a4032359967715ac91
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2022-09-22 13:57:31 +0100

    commits

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,12 +2,14 @@
 if __name__ == "__main__":
     """Print the number of and list of arguments"""
     import sys
-    lists = sys.argv
-    lengths = len(lists) - 1
-    arg = "argument"
-    print("{} argument{}{}".format(
-            lth,
-            "" if lengths == 1 else "s",
-            "." if lengths == 0 else ":"))
-    for i in range(1, len(lists)):
-        print("{}: {}".format(i, lists[i]))
+    total = len(sys.argv)
+
+    if total <= 1:
+        print("0 arguments.")
+    else:
+        if total == 2:
+            print("{} argument:".format(total - 1))
+        else:
+            print("{} arguments:".format(total - 1))
+        for i in range(1, total):
+            print("{}: {}".format(i, sys.argv[i]))[i]))
