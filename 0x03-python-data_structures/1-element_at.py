eee568c8d5c69712a6fd8757e73a0c5c762667f2
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2022-09-26 07:57:29 +0100

    commits

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-"""Retrieves an element from a list like in C."""
-
 def element_at(my_list, idx):
+    """Retrieves an element from a list like in C."""
     if idx < 0 or idx >= len(my_list):
         return
-
     return my_list[idx]
