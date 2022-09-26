4f000c382faefdef838d5d6495cc57c662ff3364
tynist <ximonaka@gmail.com>
2022-09-26 01:39:54 -0700

    commits

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     """Prints a matrix of integers."""
-    for str in matrix:
-        j = len(str)
-        if j == 0:
-            print()
-        for i in range(lenght):
-            print("{:d}".format(str[i]), end="\n" if i == j - 1 else " ")
+
+    for rows in matrix:
+        print(" ".join("{:d}".format(cols) for cols in rows))
