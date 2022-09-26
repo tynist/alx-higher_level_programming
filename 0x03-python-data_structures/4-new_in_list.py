8ce70a0ced9fc2feb5c4f47e6dd7413629fb64c9
tynist <ximonaka@gmail.com>
2022-09-26 00:39:57 -0700

    commits

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    """Replaces an element in a list at a specific position 
+    """Replaces an element in a list at a specific position
     without modifying the original list like in C."""
     if idx < 0 or idx >= len(my_list):
         return my_list.copy()
