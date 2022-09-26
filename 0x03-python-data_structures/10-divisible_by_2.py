8e659487b2a651f44e76ee5526a71d3930ed28a7
tynist <ximonaka@gmail.com>
2022-09-26 03:18:48 -0700

    commits

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    """Finds all multiples of 2 in a list and
+    Return a new list with True or False."""
+
+    if not my_list:
+        return None
+
+    div_2 = []
+    for i in my_list:
+        div_2.append(True) if i % 2 == 0 else div_2.append(False)
+    return div_2
