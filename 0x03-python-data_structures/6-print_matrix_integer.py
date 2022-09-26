0d84a87769bccd9affb747614d4a4d15472be581
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2022-09-26 09:17:16 +0100

    commits

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-"""Prints a matrix of integers."""
-
-for item in matrix:
-        l = len(item)
-        if l == 0:
-            print()
-        for i in range(l):
-            print("{:d}".format(item[i]), end="\n" if i == l - 1 else " ")
+        """Prints a matrix of integers."""
+        for item in matrix:
+                l = len(item)
+                if l == 0:
+                        print()
+                for i in range(l):
+                        print("{:d}".format(item[i]), end="\n" if i == l - 1 else " ")
