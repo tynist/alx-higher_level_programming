e34c9c5b2204333e643245d955237d214402b258
tynist <ximonaka@gmail.com>
2022-09-28 04:31:08 -0700

    commits

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    """Computes the square value of all integers of a matrix and
+    Returns a new matrix."""
+    return [list(map((lambda x: x * x), nums)) for nums in matrix]
