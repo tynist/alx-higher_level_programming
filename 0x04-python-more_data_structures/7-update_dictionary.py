ece56049f4220d6c45226bd05bcb09b828b664f8
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2022-09-28 21:54:57 +0100

    commits

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def update_dictionary(my_dict, key, value):
+def update_dictionary(a_dict, key, value):
     """Returns a new dictionary with all values multiplied by 2"""
-    my_dict[key] = value
-    return my_dict
+    a_dict[key] = value
+    return a_dict
