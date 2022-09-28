d597591d1619316964dc1bfd2971002d10a73053
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2022-09-28 15:41:56 +0100

    commits

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    """adds all unique integers in a list only once for each integer."""
-    the_list = set(my_list)
-    nums = 0
-
-    for i in the_list:
-        nums += i
-    return nums
+    """Adds all unique integers in a list only once for each integer."""
+    return sum(set(my_list))
