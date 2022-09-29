48f7b01e0d4daabea6cac00d58638a3f520ef2e6
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2022-09-29 01:42:12 +0100

    commits

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     """Converts a Roman numeral to an integer."""
-    if type(roman_string) != str or roman_string is None:
-        return 0
     roman_dict = {
         'I': 1,
         'V': 5,
@@ -11,12 +9,16 @@ def roman_to_int(roman_string):
         'C': 100,
         'D': 500,
         'M': 1000
-    }
-    
-    nums = roman_dict[i] for i in roman_string
+        }
+
+    if type(roman_string) != str or roman_string is None:
+        return 0
+
+    nums = [roman_dict[i] for i in roman_string]
     result = 0
-    for x in range(len(nums)):
-        result += nums[]
-        if nums[x - 1] < nums[x] and x != 0:
-            result -= (nums[x - 1] + nums[x - 1])
+
+    for i in range(len(nums)):
+        result += nums[i]
+        if nums[i - 1] < nums[i] and i != 0:
+            result -= (nums[i - 1] + nums[i - 1])
     return result
