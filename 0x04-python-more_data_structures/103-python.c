13212fde86c39334cf7c9a29b6a7b45bc9b87511
tynist <ximonaka@gmail.com>
2022-09-28 19:11:32 -0700

    commits

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -2,8 +2,48 @@
 #include <Python.h>
 
 /**
- * print_python_list - Prints list info
+ * print_python_bytes - Prints bytes information
  * @p: Python Object
+ * Return: no return
+ */
+void print_python_bytes(PyObject *p)
+{
+	char *string;
+	long int size, i, limit;
+
+	printf("[.] bytes object info\n");
+	if (!PyBytes_Check(p))
+	{
+		printf("  [ERROR] Invalid Bytes Object\n");
+		return;
+	}
+
+	size = ((PyVarObject *)(p))->ob_size;
+	string = ((PyBytesObject *)p)->ob_sval;
+
+	printf("  size: %ld\n", size);
+	printf("  trying string: %s\n", string);
+
+	if (size >= 10)
+		limit = 10;
+	else
+		limit = size + 1;
+
+	printf("  first %ld bytes:", limit);
+
+	for (i = 0; i < limit; i++)
+		if (string[i] >= 0)
+			printf(" %02x", string[i]);
+		else
+			printf(" %02x", 256 + string[i]);
+
+	printf("\n");
+}
+
+/**
+ * print_python_list - Prints list information
+ * @p: Python Object
+ * Return: no return
  */
 void print_python_list(PyObject *p)
 {
@@ -25,39 +65,4 @@ void print_python_list(PyObject *p)
 		if (PyBytes_Check(obj))
 			print_python_bytes(obj);
 	}
-}
-
-/**
- * print_python_bytes - Prints bytes info.
- * @p: PyObject byte
- */
-void print_python_bytes(PyObject *p)
-{
-        unsigned char i, size;
-        PyBytesObject *bytes = (PyBytesObject *)p;
-
-        printf("[.] bytes object info\n");
-        if (strcmp(p->ob_type->tp_name, "bytes") != 0)
-        {
-                printf("  [ERROR] Invalid Bytes Object\n");
-                return;
-        }
-
-        printf("  size: %ld\n", ((PyVarObject *)p)->ob_size);
-        printf("  trying string: %s\n", bytes->ob_sval);
-
-        if (((PyVarObject *)p)->ob_size > 10)
-                size = 10;
-        else
-                size = ((PyVarObject *)p)->ob_size + 1;
-
-        printf("  first %d bytes: ", size);
-        for (i = 0; i < size; i++)
-        {
-                printf("%02hhx", bytes->ob_sval[i]);
-                if (i == (size - 1))
-                        printf("\n");
-                else
-                        printf(" ");
-        }
 }
