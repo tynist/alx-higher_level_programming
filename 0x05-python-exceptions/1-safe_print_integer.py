25242f6643a24a1644b61287ae21d52fed233c98
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2022-10-10 13:09:51 +0100

    commits

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,5 +3,5 @@ def safe_print_integer(value):
     """Prints an integer with "{:d}".format()."""
     try:
         print('{:d}'.format(value))
-    except (TypeError, ValueError)
+    except (TypeError, ValueError):
         return False
