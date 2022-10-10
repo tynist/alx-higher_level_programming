6f66bc93887f76605dc64aa7a70f925dddf05d89
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2022-10-10 21:29:50 +0100

    commits

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -8,7 +8,7 @@ def magic_calculation(a, b):
                 raise Exception("Too Far")
             else:
                 result += (a ** b) / i
-        except (TypeError,ValueError):
+        except (Exception):
             result = b + a
             break
     return result
