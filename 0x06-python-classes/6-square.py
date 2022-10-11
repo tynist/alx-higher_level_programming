d6332a605d68270186a8136eaeca9847f3e2d5fa
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2022-10-11 21:50:51 +0100

    commists

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -3,55 +3,52 @@
 
 
 class Square:
-    """Class square that defines a square"""
-
+    """Square defines a geometric shape square"""
     def __init__(self, size=0, position=(0, 0)):
-        """Square with the size and position"""
+        """Constructor for Square class"""
         self.size = size
         self.position = position
 
-    def area(self):
-        """Area of the Square"""
-        return self.__size ** 2
-
-    def my_print(self):
-        """Print a Square with spaces"""
-        if self.__size == 0:
-            print()
-        else:
-            else:
-            print('\n'*self.__position[1], end='')
-            for i in range(0, self.__size):
-                print(' '*self.__position[0], end='')
-                print('#'*self.__size)
-
     @property
     def size(self):
-        """Get the private attribute size"""
+        """Get the size attribute"""
         return self.__size
 
     @size.setter
     def size(self, value):
-        """Set the size private attribute"""
-        if type(value) is not int:
+        """Set of the size attribute"""
+        if not type(value) is int:
             raise TypeError("size must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
 
     @property
     def position(self):
-        """Getter Position"""
+        """Get the position attribute"""
         return self.__position
 
     @position.setter
     def position(self, value):
-        """Setter of position"""
-        if len(value) != 2 or type(value) is not tuple \
-                or type(value[0]) is not int \
-                or type(value[1]) is not int \
+        """Set the position attribute"""
+        if not type(value) is tuple or len(value) != 2 \
+                or not type(value[0]) is int or not type(value[1]) is int \
                 or value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
+
+    def area(self):
+        """Area returns the current square area"""
+        return self.__size**2
+
+    def my_print(self):
+        """Prints the square forming by '#' symbol"""
+        if self.__size == 0:
+            print()
+        else:
+            print('\n'*self.__position[1], end='')
+            for i in range(0, self.__size):
+                print(' '*self.__position[0], end='')
+                print('#'*self.__size)
