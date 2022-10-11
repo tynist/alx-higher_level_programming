8498431eafa9221744fcffdc882a5caa3a33ceb7
tynist <ximonaka@gmail.com>
2022-10-11 06:56:52 -0700

    commits

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Square with size"""
+
+
+class Square:
+    """A class square that defines a square by its size"""
+
+    def __init__(self, square_size):
+        """Constructor of the Square"""
+        self.__size = square_size
