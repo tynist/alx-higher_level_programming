369f76cc6701c6e4f19af7d596b99a2c2b2c5f5f
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2022-10-19 15:48:22 +0100

    commits

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -12,8 +12,12 @@ def add_integer(a, b=98):
 
     Returns: addition of a and b (an integer).
     """
-    if type(a) not in [int, float]:
+    if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
-    if type(b) not in [int, float]:
+    if type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
-    return int(a) + int(b)
+    if type(a) is float:
+        a = int(a)
+    if type(b) is float:
+        b = int(b)
+    return a + b
