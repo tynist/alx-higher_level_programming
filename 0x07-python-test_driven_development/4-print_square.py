aeeb86cb5926b99877617422fd9ed07f4fb0676d
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2022-10-19 18:58:14 +0100

    Update 4-print_square.py

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -10,13 +10,11 @@ def print_square(size):
         TypeError - if size is not an integer, is a float and < 0,
         ValueError - if size < 0.
     """
-    if not isinstance(size, int):
+if type(size) != int:
         raise TypeError("size must be an integer")
-    elif size < 0:
+    if size < 0:
         raise ValueError("size must be >= 0")
-    elif isinstance(size, float) and size < 0:
-        raise TypeError("size must be an integer")
-    for i in range(0, size):
-        for j in range(size):
-            print("#", end="")
-        print("")
+    for x in range(size):
+        for y in range(size):
+            print("#", end='')
+        print()
