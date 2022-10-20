22e7f81b8a049e4a382b58201bcadbd27d759353
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2022-10-20 08:50:59 +0100

    Update 0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -12,8 +12,8 @@ def add_integer(a, b=98):
 
     Returns: addition of a and b (an integer).
     """
-    if (not isinstance(a, int) and not isinstance(a, float)):
+    if ((not isinstance(a, int) and not isinstance(a, float))):
         raise TypeError("a must be an integer")
-    if (not isinstance(b, int) and not isinstance(b, float)):
+    if ((not isinstance(b, int) and not isinstance(b, float))):
         raise TypeError("b must be an integer")
-    return int(a) + int(b)
+    return (int(a) + int(b))
