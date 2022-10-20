f42aaac696752ec70008a340169403049a23e8db
tynist <ximonaka@gmail.com>
2022-10-19 23:11:18 -0700

    commits

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -4,8 +4,11 @@
 
 def text_indentation(text):
     """
-    Args: Prints a text with 2 new lines after each of these character . ? and :
-     There should be no space at the beginning or at the end of each printed line.
+    Args: Prints a text with 2 new lines after each of these
+    character . ? and :
+    There should be no space at the beginning or at the end of
+    each printed line.
+
         :param text: the text to print (a string).
 
     Raises: TypeError - If text is not a string.
