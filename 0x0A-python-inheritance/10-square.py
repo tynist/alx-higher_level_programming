fb319dae7531d85dc77c768940b52876763b412e
tynist <ximonaka@gmail.com>
2022-10-24 14:53:13 -0700

    commits

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""A Square."""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """A class Square that inherits from a Rectangle"""
+
+    def __init__(self, size):
+        """
+        Initializing the square
+
+        Args:
+            size: size of the square (positive int)
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = siz
+
+    def area(self):
+        """Area of the Square"""
+        return self.__size ** 2
---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""A class Square that inherits Rectangle"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """class Square"""
+    def __init__(self, size):
+        """
+        Imitializimg the Square
+
+        Args:
+            size: size of square (positive int)
+        """
+        self.integer_validator(size, size)
+        super().__init__(size, size)
+        self.__size = size
