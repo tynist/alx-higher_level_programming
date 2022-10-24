c04aaf196d2497185d160d58b137119be9366e29
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2022-10-24 20:23:36 +0100

    commits

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,8 +4,6 @@
 
 def is_kind_of_class(obj, a_class):
     """
-    Returns True if the object is an instance of a class that
-    inherited (directly or indirectly) from the specified class;
-    otherwise False
+    contains the MyList class
     """
     return (issubclass(type(obj), a_class) and type(obj) != a_class)
