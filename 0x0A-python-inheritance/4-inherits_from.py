95fc1e72b30c35345c539ccb95c48d665c1e0398
tynist <ximonaka@gmail.com>
2022-10-24 12:32:29 -0700

    commits

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -8,4 +8,6 @@ def is_kind_of_class(obj, a_class):
     (directly or indirectly) from the specified class;
     otherwise False
     """
-    return (issubclass(type(obj), a_class) and type(obj) != a_class)
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    return False
