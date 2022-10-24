269f45d8853c6294f8e73e8da9c452ad8948bb0d
tynist <ximonaka@gmail.com>
2022-10-24 13:12:38 -0700

    commits

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Improves Geometry"""
+
+class BaseGeometry:
+    """class BaseGeometry"""
+    def area(self):
+        """Raises an exception"""
+        raise Exception('area() is not implemented')
