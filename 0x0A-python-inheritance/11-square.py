5a8c728c655bba66d27b5e7cd15a41cc0794bb86
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2022-10-24 23:03:14 +0100

    Update 11-square.py

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """A class Square that inherits Rectangle"""
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
