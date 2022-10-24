3a48aa8e82da7c5351f438bb69a12885c7b1ab6a
tynist <ximonaka@gmail.com>
2022-10-24 15:59:34 -0700

    commits

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -6,8 +6,9 @@ def is_kind_of_class(obj, a_class):
     """
     Returns True if the object is an instance of a class that inherited
     (directly or indirectly) from the specified class;
-    otherwise False
+    otherwi
     """
-    if issubclass(type(obj), a_class) and type(obj) != a_class:
+
+if issubclass(type(obj), a_class) and type(obj) != a_class:
         return True
     return False
