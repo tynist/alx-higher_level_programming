d8750cbf7b21a57aeb2e12b2501b1145b718eda6
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2022-11-29 21:47:02 +0100

    12-no_cheating

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,3 +1,3 @@
--- script that updates the score of Bob to 10 in the table second_table.
+-- script that updates the score of Bob to 10 in the table second_table
 UPDATE `second_table`
 SET `score` = "10" WHERE `second_table`.`name` = "Bob"
