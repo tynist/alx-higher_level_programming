c0506aab386a668823da305f366cd286cb9a8a06
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2022-11-30 19:59:22 +0100

    Update 16-shows_by_genre.sql

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -4,4 +4,4 @@ SELECT s.title, g.name
 FROM tv_shows s
 LEFT JOIN tv_show_genres m ON s.id = m.show_id
 LEFT JOIN tv_genres g ON m.genre_id = g.id
-ORDER BY s.title ASC;
+ORDER BY s.title, g.name ASC;
