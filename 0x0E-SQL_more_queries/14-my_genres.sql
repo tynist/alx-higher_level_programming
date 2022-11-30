5a168dde87a66e541aee18d223d4da0aa71fb624
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2022-11-30 19:40:29 +0100

    14-my_genres

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,8 @@
+-- Use 'hbtn_0d_tvshows' to list all genres of show 'Dexter'
+-- 'tv_shows' table contains only one record where title = Dexter
+SELECT tv_genres.name
+FROM tv_genres
+INNER JOIN tv_show_genres m ON tv_genres.id = m.genre_id
+INNER JOIN tv_shows s ON m.show_id = s.id
+WHERE s.title = 'Dexter'
+ORDER BY tv_genres.name ASC;
