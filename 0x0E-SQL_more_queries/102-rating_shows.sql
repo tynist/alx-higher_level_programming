a4128e2b54429c469e3c0b20ac33c0fe7e60bb01
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2022-11-30 20:04:27 +0100

    102-rating_shows

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,7 +1,5 @@
 -- List all shows from 'hbtn_0d_tvshows_rate' by their rating
 -- Each record should display tv_shows.title, rating sum
--- Results must be sorted in ascending order by the rating
--- You can use only one SELECT statement
 SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating
 FROM tv_shows
 INNER JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
