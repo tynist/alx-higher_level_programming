f0ab9cf80f8df1fe14828ccad987593307f4ed99
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2022-11-30 19:50:47 +0100

    16-shows_by_genre

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- List all shows and genres linked to show from 'hbtn_0d_tvshows', If show doesn't have a genre, display NULL in genre column
+-- Each record should display tv_shows.title, tv_genres.name
+SELECT s.title, g.name
+FROM tv_shows s
+LEFT JOIN tv_show_genres m ON s.id = m.show_id
+LEFT JOIN tv_genres g ON m.genre_id = g.id
+ORDER BY s.title ASC;
