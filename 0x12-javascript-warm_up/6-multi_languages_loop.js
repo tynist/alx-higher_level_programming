893db2857aabb1dbf0ebf6a8eaeb81ce11b054b2
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2022-12-19 22:18:18 +0100

    6-multi_languages_loop

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,4 @@
 #!/usr/bin/node
-const array = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (let index = 0; index < array.length; index++) {
-  const element = array[index];
-  console.log(element);
+for (const line of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
+  console.log(line);
 }
