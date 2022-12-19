59372a115eb4f87cf91c3178bd46a553c388f646
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2022-12-19 22:04:10 +0100

    10-factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-function factorial (num) {
-  if (!num || num < 2) {
+function factorial (nbr) {
+  if (!nbr || nbr < 2) {
     return (1);
   }
-  return num * factorial(num - 1);
+  return nbr * factorial(nbr - 1);
 }
 console.log(factorial(Number(process.argv[2])));
