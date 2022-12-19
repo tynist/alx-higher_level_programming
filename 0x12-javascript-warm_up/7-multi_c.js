9523b983a40e328a10f904c52c2af61d612b2cc3
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2022-12-19 22:22:24 +0100

    7-multi_c

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const occurences = Number(process.argv[2]);
 if (occurences) {
-  for (let index = 0; index < occurences; index++) {
+  for (let i = 0; i < occurences; i++) {
     console.log('C is fun');
   }
 } else {
