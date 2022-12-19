9406319c12c21f4e07d98459bdc804f7e7d2b7aa
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2022-12-19 22:24:33 +0100

    8-square.

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const size = Number(process.argv[2]);
 if (size) {
-  for (let index = 0; index < size; index++) {
+  for (let i = 0; i < size; i++) {
     console.log('X'.repeat(size));
   }
 } else {
