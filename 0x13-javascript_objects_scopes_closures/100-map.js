74801b88af1699bc528132a64134aabd23444ac2
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2022-12-20 20:46:07 +0100

    100-map

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const { list } = require('./100-data');
-const newList = list.map((index, value) => index * value);
+const newList = list.map((i, nbr) => i * nbr);
 console.log(list);
-console.log(newList);.
+console.log(newList);
