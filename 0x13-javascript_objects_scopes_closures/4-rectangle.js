c1a36dd88bcd51d8560a393af5637d72f9ee335f
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2022-12-20 20:23:02 +0100

    4-rectangle

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -8,7 +8,7 @@ class Rectangle {
   }
 
   print () {
-    for (let index = 0; index < this.height; index++) {
+    for (let i = 0; i < this.height; i++) {
       console.log('X'.repeat(this.width));
     }
   }
