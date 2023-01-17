8054805086d4c45fec6c0c43d3550a13ff846f34
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-01-17 13:04:25 +0100

    Update 6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -8,6 +8,6 @@ import requests
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    payload = {'email': sys.argv[2]}
-    r = requests.post(url, data=payload)
+    value = {'email': sys.argv[2]}
+    r = requests.post(url, data=value)
     print(r.text)
