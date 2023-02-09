48c57146f0a3efe637f4ac3ea0e3cb6d1267b948
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-02-09 13:22:24 +0100

    Update 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
-// Script prints the title of a Star Wars movie where 
-// the episode number matches a given integer.
-
 const request = require('request');
 const starWarsUri = 'https://swapi-api.hbtn.io/api/films/'.concat(process.argv[2]);
 request(starWarsUri, function (error, response, body) {
