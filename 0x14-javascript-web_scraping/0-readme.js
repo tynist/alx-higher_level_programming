663d5b50a6eaf0af97593eed8514931846c8a466
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-02-09 09:48:08 +0100

    Update 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-# script reads and prints the content of a file.
+// script reads and prints the content of a file.
 
 const fs = require('fs');
 fs.readFile(process.argv[2], 'utf8', function (error, content) {
